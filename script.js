790f356edf0db50257f7acc26afbde371af02a69
Stepan Bashkeev <92249523+Stepan23dm@users.noreply.github.com>
2022-11-10 12:52:46 +0700

    Make lab 7 part 1

---
+++ script.js
@@ -0,0 +1,30 @@
+let firstValue = 2;
+let secondValue = 5;
+
+console.log(firstValue + " --- " + secondValue);
+
+let sum = firstValue + secondValue;
+console.log(sum);
+
+let str = "JaVaScriPT";
+let upperCase = str.toUpperCase();
+console.log("Upper Case - " + upperCase);
+let lowerCase = str.toLowerCase();
+console.log("Lower Case - " + lowerCase);
+
+let plus = firstValue + secondValue;
+let minus = secondValue - firstValue;
+let umnoj = secondValue * secondValue;
+let delen = secondValue / firstValue;
+let kvad = Math.pow(firstValue, 2);
+let ost = secondValue % firstValue;
+
+console.log("Сложенение: " + plus);
+console.log("Вычитание: " + minus);
+console.log("Умножение: " + umnoj);
+console.log("Деление: " + delen);
+console.log("Возведение в квадрат: " + kvad);
+console.log("Остаток от деления: " + ost);
+
+
+
